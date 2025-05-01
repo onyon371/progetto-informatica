11506f4b297ff0511fb67e82fc014f37cae9d625
onyon371 <onyon371@gmail.com>
2025-05-01 10:48:02 +0200

    mergiato due implementazini stessa view

--- src/main/java/com/example/progetto_informatica/savedPilotsController.java
+++ src/main/java/com/example/progetto_informatica/savedPilotsController.java
@@ -49,6 +49,44 @@ public class savedPilotsController implements Initializable {
         }
     }
 
+    /*@FXML
+    void addPilot(ActionEvent event) {
+        // Create dialog to get pilot details
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("Aggiungi Pilota");
+        dialog.setHeaderText("Inserisci i dati del pilota che vuoi aggiungere!");
+        dialog.setContentText("Nome Pilota:");
+        dialog.setContentText("Cognome Pilota:");
+
+        Optional<String> pilot = dialog.showAndWait();
+
+        if(!pilot.isEmpty()) {
+            handlePilotCard("","");
+
+        }
+
+    }
+
+    @FXML
+    void removePilot(ActionEvent event) {
+
+    }
+    @FXML
+    void handlePilotCard(String name, String surname){
+        VBox tournamentContainer = new VBox();
+        tournamentContainer.getStyleClass().add("tournament-container");
+        tournamentContainer.setSpacing(5);
+
+        // Add separator if not the first pilot
+        if (pilotContainer.getChildren().size() > 1) {
+            Separator separator = new Separator();
+            separator.getStyleClass().add("separator");
+            pilotContainer.getChildren().add(1, separator); // Add after the new pilot
+        }
+    }*/
+
+
+
     private void addPilotsCard() {
         pilotsAnchor.getChildren().clear();
 
